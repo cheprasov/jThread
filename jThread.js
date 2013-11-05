e8ba0d3fa982b6646acde366f48cb8ceb4c2f5d4
cheprasov <alexander@cheprasov.com>
2013-11-05 03:42:10 -0800

    Update jThread.js

--- jThread.js
+++ jThread.js
@@ -4,7 +4,11 @@
 (function(){
 	"use strict";
 
-	var jThread = window.jThread = function ( thread, done ) {
+	var jThread = window.jThread = function ( thread, done, config ) {
+
+		config = config || {
+			once : false
+		}
 
 		if ( this instanceof jThread ) return jThread(thread, done);
 
@@ -13,11 +17,18 @@
 			|| ! done || typeof(done) !== 'function'
 		) return null;
 
-		if (! window.Worker || ! window.URL || ! window.URL.createObjectURL || ! window.Blob ) {
-			
+		if (1 || ! window.Worker || ! window.URL || ! window.URL.createObjectURL || ! window.Blob ) {
+
 			//return simple async function
-			return function ( msg ) {
-				setTimeout(function(){done(thread(msg))},1);
+			var times = 0;
+			return function ( ) {
+				var args = Array.prototype.slice.call(arguments);
+				if ( config.once && times ) return ;
+				setTimeout(function(){
+					if ( config.once && times ) return ;
+					times += 1;
+					done ( thread.apply(thread, args), 'timer' )
+				},1);
 			}//fun
 
 		}//if
@@ -25,22 +36,25 @@
 		var worker = new Worker (
 				window.URL.createObjectURL(
 					new Blob(
-						['onmessage = function(w){ postMessage({status:"done",result:(' + Function.toString.call(thread)+'(w.data))}); };']
+						['self.onmessage = function ( wrk ) { var f = '+ Function.toString.call(thread) +'; self.postMessage( { status : "done", result : f.apply(f, wrk.data.args ) } ); '+ ( config.once ? ' self.close(); ' : '' ) +' };']
 						,{type : 'text/javascript'}
 					)
 				)
 			);
 
-		worker.onmessage = function ( w ) {
-			if ( typeof(w.data) === 'object' && w.data.status === 'done' ) {
-				done (w.data.result, w.data.status);
+		worker.onmessage = function ( wrk ) {
+			if ( typeof(wrk.data) === 'object' && wrk.data.status ) {
+				done (wrk.data.result, wrk.data.status);
 			} else {
-				done (w.data);
+				done (wrk.data);
 			}//if
 		}//fun
 
-		return function ( msg ) {
-			worker.postMessage(msg);
+		return function (/* args */) {
+			var obj = {
+				args : Array.prototype.slice.call(arguments)
+			};
+			worker.postMessage(obj);
 		}//fun
 
 	}//fun
